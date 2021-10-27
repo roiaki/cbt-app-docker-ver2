a5199f319965a8ae9b1039b9a98ad0ca700b1204
roiaki <roitoitoitoi@yahoo.co.jp>
2021-10-27 14:37:53 +0900

    7コラム編集ページレイアウト調整

--- resources/views/seven_columns/edit.blade.php
+++ resources/views/seven_columns/edit.blade.php
@@ -17,7 +17,7 @@
 
     <div class="form-group">
       <!-- タイトル -->
-      {!! Form::label('title', 'タイトル') !!}
+      {!! Form::label('title', '①-1  タイトル') !!}
       {!! Form::text('title', null, ['class' => 'form-control']) !!}
 
       <!-- タイトル必須バリデーション表示 課題：まとめてかけないか-->
@@ -32,7 +32,7 @@
 
     <div class="form-group">
       <!-- 内容 -->
-      {!! Form::label('content', '内容') !!}
+      {!! Form::label('content', '①-2  内容') !!}
       {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => 3] ) !!}
 
       <!-- 内容必須バリデーション表示-->
@@ -47,16 +47,18 @@
 
 
     <div class="form-group">
-      <label for="emotion-name">感情名</label>
+      <label for="emotion-name">②-1  感情名</label>
       <input type="text" class="form-control" name="emotion_name" value="{{ $seven_column->emotion_name }}">
-
-      <label for="emotion-strength">強さ</label>
+    </div>
+    
+    <div class="form-group">
+      <label for="emotion-strength">②-2  強さ</label>
       <input type="number" class="form-control" name="emotion_strength" value="{{ $seven_column->emotion_strength }}">
     </div>
 
 
     <div class="form-group">
-      <label for="thinking">その時考えたこと</label><br>
+      <label for="thinking">③ その時考えたこと</label><br>
       <textarea class="form-control" id="thinking" name="thinking" cols="50" rows="3">{{ $seven_column->thinking }}</textarea>
       <!-- 内容必須バリデーション表示-->
       @if($errors->has('thinking'))
@@ -68,17 +70,24 @@
       @endif
     </div>
 
-    <label for="basis_thinking">考えの根拠</label>
-    <textarea class="form-control" id="basis_thinking" name="basis_thinking" cols="50" rows="3">{{ $seven_column->basis_thinking }}</textarea>
+    <div class="form-group">
+      <label for="basis_thinking">④  考えの根拠</label>
+      <textarea class="form-control" id="basis_thinking" name="basis_thinking" cols="50" rows="3">{{ $seven_column->basis_thinking }}</textarea>
+    </div>
 
-    <label for="opposite_fact">逆の事実</label>
-    <textarea class="form-control" id="opposite_fact" name="opposite_fact" cols="50" rows="3">{{ $seven_column->opposite_fact }}</textarea>
+    <div class="form-group">
+      <label for="opposite_fact">⑤  逆の事実</label>
+      <textarea class="form-control" id="opposite_fact" name="opposite_fact" cols="50" rows="3">{{ $seven_column->opposite_fact }}</textarea>
+    </div>
+    <div class="form-group">
+      <label for="new_thinking">⑥  新しい考え</label>
+      <textarea class="form-control" id="new_thinking" name="new_thinking" cols="50" rows="3">{{ $seven_column->new_thinking }}</textarea>
+    </div>
 
-    <label for="new_thinking">新しい考え</label>
-    <textarea class="form-control" id="new_thinking" name="new_thinking" cols="50" rows="3">{{ $seven_column->new_thinking }}</textarea>
-
-    <label for="new_emotion">新しい感情</label>
-    <textarea class="form-control" id="new_emotion" name="new_emotion" cols="50" rows="3">{{ $seven_column->new_emotion }}</textarea>
+    <div class="form-group">
+      <label for="new_emotion">⑦  新しい感情</label>
+      <textarea class="form-control" id="new_emotion" name="new_emotion" cols="50" rows="3">{{ $seven_column->new_emotion }}</textarea>
+    </div>
 
     <div class="buttons-first">
       {!! Form::submit('更新', ['class' => 'btn btn-primary btn-lg']) !!}
