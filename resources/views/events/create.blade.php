4860ce629ba95d94d5cf06e21e67d7e639f591ed
roiaki <roitoitoitoi@yahoo.co.jp>
2022-04-08 21:52:32 +0900

    レイアウトと文言を変更

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,49 +2,50 @@
 
 @section('content')
 
-<h3>出来事　新規作成</h3>
-
-<div class="row">
+<div class="row justify-content-center">
   <div class="col-sm-7">
-    <form action="{{ route('events.store') }}" method="post">
-      @csrf
-      <div class="form-group">
-       
-        <label for="title">出来事 の タイトル</label>
-        <input type="text" class="form-control" id="title" name="title" value = "{{ old('title') }}">
+    
+    <h3 class="title_head">出来事　新規作成</h3>
 
-        <!-- バリデーションエラー表示-->
-        @if($errors->has('title'))
-        @foreach($errors->get('title') as $message)
-        <ul>
-          <li class="ml-2 my-1 text-danger">{{ $message }}</li>
-        </ul>
-        @endforeach
-        @endif
-      </div>
+      <form action="{{ route('events.store') }}" method="post">
+        @csrf
+        <div class="form-group">
+        
+          <label class="heading" for="title">出来事 の タイトル</label>
+          <input type="text" class="form-control" id="title" name="title" value = "{{ old('title') }}">
 
-      <div class="form-group">
-        <!-- 内容 -->
-        <label for="content">出来事 の 内容</label>
-        <textarea class="form-control" id="content" name="content" cols="90" rows="7">{{ old('content') }}</textarea>
+          <!-- バリデーションエラー表示-->
+          @if($errors->has('title'))
+          @foreach($errors->get('title') as $message)
+          <ul>
+            <li class="ml-2 my-1 text-danger">{{ $message }}</li>
+          </ul>
+          @endforeach
+          @endif
+        </div>
 
-        <!-- バリデーションエラー表示-->
-        @if($errors->has('content'))
-        @foreach($errors->get('content') as $message)
-        <ul>
-          <li class="ml-2 my-1 text-danger">{{ $message }}</li>
-        </ul>
-        @endforeach
-        @endif
-      </div>
+        <div class="form-group">
+          <!-- 内容 -->
+          <label class="heading" for="content">出来事 の 内容</label>
+          <textarea class="form-control" id="content" name="content" cols="90" rows="7">{{ old('content') }}</textarea>
 
-      <input type="submit" value="出来事作成" class="btn btn-primary btn-lg"> 
-      
-      <div class="buttons">
-        <button type="button" class="btn btn-secondary btn-lg" onclick="history.back(-1)">戻る</button>
-      </div>
-      
-    </form>
+          <!-- バリデーションエラー表示-->
+          @if($errors->has('content'))
+          @foreach($errors->get('content') as $message)
+          <ul>
+            <li class="ml-2 my-1 text-danger">{{ $message }}</li>
+          </ul>
+          @endforeach
+          @endif
+        </div>
+
+        <input type="submit" value="出来事作成" class="btn btn-primary btn-lg"> 
+        
+        <div class="buttons">
+          <button type="button" class="btn btn-secondary btn-lg" onclick="history.back(-1)">戻る</button>
+        </div>
+        
+      </form>
 
   </div>
 </div>
